7c24c933928ded999f537e46c6390eb293c6a4a5
akash-r-prabhu <akashprabhu41@gmail.com>
2021-08-29 14:56:34 +0530

    enter

--- Class and objects/1.cpp
+++ Class and objects/1.cpp
@@ -1,25 +1,51 @@
 #include <iostream>
-#include <string.h>
+#include <string.h> 
 using namespace std;
 class student
 {
     int roll;
     char name[20];
-    
-    public:
-        student(){roll=0;strcpy(name,"akash");}
-        int retroll()
-            {
-                return roll;
-            }
-        char* retname()
-            {
-                return name;
-            } 
-}s;
+
+public:
+    student()
+    {
+        roll = 0;
+        strcpy(name, "");
+    }
+    int setrollandname(int r, char n[20])
+    {
+        roll = r;
+        strcpy(name, n);
+        return 0;
+    }
+    int retroll() //To return roll no
+    {
+        return roll;
+    }
+    char *retname() //to return name
+    {
+        return name;
+    }
+
+} s;
 int main()
 {
-    cout<<s.retroll()<<s.retname();
+    int choice, roll;
+    char name[20];
+    cout << "enter 1 if u want to enter roll number and name";
+    cin >> choice;
+    if (choice == 1)
+    {
+        cout << "\nenter roll no  : ";
+        cin >> roll;
+        cout<<"\nenter name : ";
+        cin>>name;
+        int c;
+        s.setrollandname(roll, name);
+    }
 
+    cout << "\n"
+         <<"Roll no = " <<s.retroll() <<"\n"<<"Name : "<< s.retname();
 
+    return 0;
 }
